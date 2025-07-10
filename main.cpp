278521e3b2f1fbb9ccaae437729eed69a95fdd6a
Maria Akter <aktermariarimi@gmail.com>
2025-07-10 12:15:25 +0600

    animated the knight

--- main.cpp
+++ main.cpp
@@ -17,6 +17,17 @@ int main(){
         (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
     };
 
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+    // 1: facing right, -1 : facing left
+    float rightLeft{1.f};
+
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f/12.f};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -33,17 +44,31 @@ int main(){
        {
         // set mapPos = mapPos - direction
         mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        knight = knight_run;
+       } else {
+        knight = knight_idle;
        }
        
        
        // draw the map
        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
+       // update animation frame
+       runningTime += GetFrameTime();   // running time +=  delta time (dt)
+       if (runningTime >= updateTime)
+       {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrames) frame = 0;
+       }
+       
+
        // draw the character
-       Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+       Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
-       
+
        EndDrawing();
     }
     CloseWindow();;
